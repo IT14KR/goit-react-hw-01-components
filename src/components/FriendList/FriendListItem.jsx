358018f719ec11b26b1ef8320dba75e0ecc78064
IT14KR <131267129+IT14KR@users.noreply.github.com>
2023-08-21 18:31:36 +0200

    Update FriendListItem.jsx

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,7 +3,7 @@ import { Item, Status, Avatar, Name } from './FriendList.styled';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => (
   <Item>
-    <Status></Status>
+    <Status statusType={isOnline}></Status>
     <Avatar src={avatar} alt="User avatar" width="48" />
     <Name>{name}</Name>
   </Item>
